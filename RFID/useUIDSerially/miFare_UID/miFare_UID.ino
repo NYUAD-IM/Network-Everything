58ac1db8770be43c50c1499d506ea693b14b5053
Scott Fitzgerald <scott@droolcup.com>
2016-02-17 13:57:17 +0400

    changed SPI pins

--- RFID/useUIDSerially/miFare_UID/miFare_UID.ino
+++ RFID/useUIDSerially/miFare_UID/miFare_UID.ino
@@ -1,11 +1,14 @@
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
-// Use this line for a breakout with a hardware SPI connection.  Note that
-// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
-// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
-// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
-Adafruit_PN532 nfc(PN532_SS);
+// If using the breakout with SPI, define the pins for SPI communication.
+#define PN532_SCK  (2)
+#define PN532_MOSI (3)
+#define PN532_SS   (4)
+#define PN532_MISO (5)
+
+// Use this line for a breakout with a SPI connection:
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
 long lastID = 0; // previous UID number
 
